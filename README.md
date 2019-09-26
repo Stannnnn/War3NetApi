e1ac826cf5b661b79aaea5a3be861515a28492e5
Stannnnn <stann@live.nl>
2019-09-26 20:21:02 +0200

    v1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # War3NetApi
+
+CLI API for https://github.com/Drake53/War3Net/tree/master/src/War3Net.IO.Mpq
+
+Example: War3NetMPQApi.exe "restore>abc.w3x" "open>abc.w3x" "extract>war3map.j>war3map.j"
---
+++ War3NetApi/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using War3Net.IO.Mpq;
+
+namespace War3NetApi
+{
+    class Program
+    {
+        private static MpqArchive mpqArchive;
+        private static FileStream mpqArchiveStream;
+
+        private static void Open(string fileName) {
+            mpqArchiveStream = File.OpenRead(fileName);
+            mpqArchive = new MpqArchive(mpqArchiveStream);
+        }
+
+        private static void Extract (string fileName, string fileOut) {
+            if (mpqArchive != null)
+            {
+                using (MpqStream fileStreamIn = mpqArchive.OpenFile(fileName))
+                {
+                    using (FileStream fileStreamOut = File.OpenWrite(fileOut))
+                    {
+                        fileStreamIn.CopyTo(fileStreamOut);
+                    }
+                }
+            }
+        }
+
+        private static void Unh3x(string file) {
+            using (MemoryStream stream = MpqArchive.Restore(file))
+            {
+                using (FileStream streamOut = new FileStream(file, FileMode.Create))
+                {
+                    stream.CopyTo(streamOut);
+                }
+            }
+        }
+
+        private static void Replace(string file, string replaceFile)
+        {
+            using (FileStream stream = File.OpenRead(replaceFile)) { 
+                mpqArchive.ReplaceFile(file, stream);
+            }
+        }
+
+        private static void Close()
+        {
+            if (mpqArchiveStream != null)
+            {
+                mpqArchiveStream.Dispose();
+            }
+
+            if (mpqArchive != null)
+            {
+                mpqArchive.Dispose();
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            foreach (string arg in args) {
+                var parts = arg.Split('>');
+
+                switch (parts[0])
+                {
+                    case "open":
+                        Close();
+                        Open(parts[1]);
+                        break;
+
+                    case "extract":
+                        Extract(parts[1], parts[2]);
+                        break;
+
+                    case "restore":
+                        Unh3x(parts[1]);
+                        break;
+
+                    case "replace":
+                        Replace(parts[1], parts[2]);
+                        break;
+
+                    case "close":
+                        Close();
+                        break;
+                }
+            }
+
+            Close();
+        }
+    }
+}
