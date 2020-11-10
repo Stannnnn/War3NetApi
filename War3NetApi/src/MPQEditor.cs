19471623678003cb63adc6a7096c8d156376a005
Stannnnn <stann@live.nl>
2020-11-10 19:57:47 +0100

    Changes

---
+++ War3NetApi/src/MPQEditor.cs
@@ -0,0 +1,77 @@
+﻿#nullable enable
+
+using System.IO;
+using War3Net.IO.Mpq;
+
+namespace War3NetMPQApi
+{
+    class MPQEditor
+    {
+        private MpqArchiveFiles? mpqArchiveFiles;
+        private string? mpqArchivePath;
+
+        public void Open(string fileName)
+        {
+            mpqArchivePath = fileName;
+
+            var copy = new MemoryStream();
+            using (var filestream = File.OpenRead(fileName))
+            {
+                filestream.CopyTo(copy);
+            }
+            copy.Position = 0;
+
+            mpqArchiveFiles = new MpqArchiveFiles(copy);
+        }
+
+        public void Extract(string fileName, string fileOut)
+        {
+            if (mpqArchiveFiles != null)
+            {
+                try
+                {
+                    using (MpqStream fileStreamIn = mpqArchiveFiles.MpqArchive.OpenFile(fileName))
+                    {
+                        using (FileStream fileStreamOut = File.Create(fileOut))
+                        {
+                            fileStreamIn.CopyTo(fileStreamOut);
+                        }
+                    }
+                }
+                catch (FileNotFoundException) { }
+            }
+        }
+
+        public void Unh3x(string file)
+        {
+            using (MemoryStream stream = MpqArchive.Restore(file))
+            {
+                using (FileStream streamOut = File.Create(file))
+                {
+                    stream.CopyTo(streamOut);
+                }
+            }
+        }
+
+        public void Replace(string inputFile, string replaceFile)
+        {
+            mpqArchiveFiles?.ModifiedFiles.Add(MpqFile.New(File.OpenRead(inputFile), replaceFile));
+        }
+
+        public void Remove(string file)
+        {
+            mpqArchiveFiles?.RemovedFiles.Add(MpqHash.GetHashedFileName(file));
+        }
+
+        public void Close()
+        {
+            if (mpqArchivePath != null)
+            {
+                mpqArchiveFiles?.SaveTo(mpqArchivePath);
+                mpqArchivePath = null;
+            }
+
+            mpqArchiveFiles?.Dispose();
+        }
+    }
+}
---
+++ War3NetApi/src/Program.cs
@@ -0,0 +1,47 @@
+﻿#nullable enable
+
+namespace War3NetMPQApi
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            MPQEditor mpqEditor = new MPQEditor();
+
+            foreach (string arg in args)
+            {
+                var parts = arg.Split('>');
+
+                switch (parts[0])
+                {
+                    case "open":
+                        mpqEditor.Close();
+                        mpqEditor.Open(parts[1]);
+                        break;
+
+                    case "extract":
+                        mpqEditor.Extract(parts[1], parts[2]);
+                        break;
+
+                    case "restore":
+                        mpqEditor.Unh3x(parts[1]);
+                        break;
+
+                    case "replace":
+                        mpqEditor.Replace(parts[1], parts[2]);
+                        break;
+
+                    case "remove":
+                        mpqEditor.Remove(parts[1]);
+                        break;
+
+                    case "close":
+                        mpqEditor.Close();
+                        break;
+                }
+            }
+
+            mpqEditor.Close();
+        }
+    }
+}
